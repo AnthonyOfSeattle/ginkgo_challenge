1e073c4ee29dc99fd0633a0cca47f39a7f6acce9
Anthony Valente <valenta4@uw.edu>
2022-05-18 09:53:46 -0700

    Add no input flag to production build script.

--- buildprod.sh
+++ buildprod.sh
@@ -1,4 +1,4 @@
 docker compose -f docker-compose.prod.yml up -d --build
 docker compose exec app python manage.py buildgenomebank
-docker compose exec app python manage.py migrate
-docker compose exec app python manage.py collectstatic
+docker compose exec app python manage.py migrate --no-input
+docker compose exec app python manage.py collectstatic --no-input
